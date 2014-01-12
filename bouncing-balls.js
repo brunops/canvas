c730ed2bdbed43450423a1b243d8b41f6709e46e
Bruno Sanches <brunopsanches@gmail.com>
2014-01-12 02:19:37 -0800

    Define Ball model
    * It knows:
    	- its coordinates
    	- its color
    	- its radius
    	- how to draw itself

---
+++ bouncing-balls.js
@@ -0,0 +1,20 @@
+window.onload = (function() {
+
+  function Ball(x, y, radius, color) {
+    this.init();
+  }
+
+  Ball.prototype.init = function(x, y, radius, color) {
+    this.x = x;
+    this.y = y;
+    this.radius = radius;
+    this.color = color;
+  };
+
+  Ball.prototype.draw = function(context) {
+    context.beginPath();
+    context.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
+    context.fill();
+  };
+
+})();
