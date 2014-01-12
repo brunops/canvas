e33c34b051eb82a824ab6452145240cda56f8e95
Bruno Sanches <brunopsanches@gmail.com>
2014-01-12 02:46:25 -0800

    Draw Balls when canvas is clicked
    * Create BouncingBalls collection
    * Define BouncingBalls#add that creates a new Ball and adds it to the Collection
    * Define BouncingBalls#draw to draw all balls into its canvas

--- bouncing-balls.js
+++ bouncing-balls.js
@@ -1,7 +1,7 @@
 window.onload = (function() {
 
   function Ball(x, y, radius, color) {
-    this.init();
+    this.init(x, y, radius, color);
   }
 
   Ball.prototype.init = function(x, y, radius, color) {
@@ -19,4 +19,40 @@ window.onload = (function() {
     context.fill();
   };
 
+  function BouncingBalls() {
+    this.init();
+  }
+
+  BouncingBalls.prototype.init = function() {
+    this.collection = [];
+
+    this.canvas  = document.getElementById('canvas');
+    this.context = canvas.getContext('2d');
+
+    this.bindEvents();
+
+    setInterval(this.draw.bind(this), 500);
+  };
+
+  BouncingBalls.prototype.bindEvents = function() {
+    var self = this;
+
+    this.canvas.addEventListener('click', function(e) {
+      self.add(e.pageX - self.canvas.offsetLeft, e.pageY - self.canvas.offsetTop, 5, 'red');
+    });
+  };
+
+  BouncingBalls.prototype.add = function(x, y, radius, color) {
+    console.log(x, y, radius, color);
+    this.collection.push(new Ball(x, y, radius, color));
+  };
+
+  BouncingBalls.prototype.draw = function() {
+    for (var i = 0; i < this.collection.length; ++i) {
+      console.log('draw', this.collection[i]);
+      this.collection[i].draw(this.context);
+    }
+  };
+
+  new BouncingBalls();
 })();
