422413481082d0e20c1dd9efd7859345e9a1e13a
Bruno Sanches <brunopsanches@gmail.com>
2014-01-12 02:22:30 -0800

    Define Ball fillStyle using its color; close draw path

--- bouncing-balls.js
+++ bouncing-balls.js
@@ -12,8 +12,10 @@ window.onload = (function() {
   };
 
   Ball.prototype.draw = function(context) {
+    context.fillStyle = this.color;
     context.beginPath();
     context.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
+    context.closePath();
     context.fill();
   };
 
