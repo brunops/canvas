583db06f256e3fb7179cd60178323b2397630113
Bruno Sanches <brunopsanches@gmail.com>
2014-01-22 02:20:34 -0800

    Tower of Hanoi - Handle some validation messages for invalid numbers and reestructure layout a bit

--- tower-of-hanoi/main.js
+++ tower-of-hanoi/main.js
@@ -2,13 +2,28 @@
   var defaultNumberOfDisks = 6;
 
   var updateButton = document.getElementById('update-disks-btn'),
-      input        = document.getElementById('total-disks');
+      input        = document.getElementById('total-disks'),
+      controls     = document.getElementById('controls'),
+      errorMsg     = document.getElementById('error-msg');
 
   updateButton.addEventListener('click', function(e) {
-    var newTotal = parseInt(input.value, 10);
+    var isValidNumber = true,
+        newTotal = parseInt(input.value, 10);
+
+    hideMessages();
 
     // take care of smart-asses
-    if (isNaN(newTotal) || newTotal > 10 || newTotal < 1) {
+    if (isNaN(newTotal) || newTotal < 1 || Number(newTotal).toString() !== input.value) {
+      displaySmartyMsg();
+      isValidNumber = false;
+    }
+    else if (newTotal > 10) {
+      displayMaxValueMsg();
+      isValidNumber = false;
+    }
+
+    // Reset to default value
+    if (!isValidNumber) {
       newTotal = defaultNumberOfDisks;
       input.value = defaultNumberOfDisks;
     }
@@ -17,6 +32,22 @@
 
     e.preventDefault();
   });
+
+  function setMessage(msg) {
+    errorMsg.innerHTML = '<span>' + msg + '</span>';
+  }
+
+  function displaySmartyMsg() {
+    setMessage('Value reset to its default because you are so smart :)');
+  }
+
+  function displayMaxValueMsg() {
+    setMessage('Max allowed number is 10 - don\'t spend eternity here..');
+  }
+
+  function hideMessages() {
+    errorMsg.innerHTML = '';
+  }
 
   function startAnimation(totalDisks) {
     TowerAnimation.animate(document.getElementById('canvas'), totalDisks);
